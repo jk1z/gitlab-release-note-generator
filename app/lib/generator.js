266fffede3bde4646187da87700591ca69b5e24a
Jack Zhang <jackzhang0096@gmail.com>
2019-06-01 16:26:19 +1000

    Minor fixes on start and end date

--- app/lib/generator.js
+++ app/lib/generator.js
@@ -11,8 +11,8 @@ exports.generate = async () => {
 
     if (latestTag) {
         Logger.info(`Latest tag is ${latestTag.name}`);
-        const [startDate, endDate] = await TagLib.getDateRangeFromLatestAndSecondLatestTagByProjectId(Env.GITLAB_PROJECT_ID);
-        Logger.info(`Time range that we are looking at MRs and issues is between ${Moment.tz(startDate, Env.TZ).format()} and ${Moment.tz(endDate, Env.tz).format()}`);
+        const [endDate, startDate] = await TagLib.getDateRangeFromLatestAndSecondLatestTagByProjectId(Env.GITLAB_PROJECT_ID);
+        Logger.info(`Time range that we are looking at MRs and issues is between ${startDate} and ${endDate}`);
         const mergeRequests = await MergeRequestLib.getMergeRequestByProjectIdStateStartDateAndEndDate(Env.GITLAB_PROJECT_ID, "merged", startDate, endDate);
         Logger.info(`Found ${mergeRequests ? mergeRequests.length : 0} merge requests`);
         const issues = await IssueLib.searchIssuesByProjectIdStateStartDateAndEndDate(Env.GITLAB_PROJECT_ID, "closed", startDate, endDate);
