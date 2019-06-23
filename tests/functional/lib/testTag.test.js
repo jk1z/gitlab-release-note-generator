412ae1af5619ae9c677f3c9ca4e761ba809dca0d
Jack Zhang <jack.zhang@medipass.io>
2019-06-23 02:15:19 -0700

    WIP

--- tests/functional/lib/testTag.test.js
+++ tests/functional/lib/testTag.test.js
@@ -1,13 +1,90 @@
 const Env = require("../../../app/env");
-const Nock = require("nock");
 const Tag = require("../../../app/lib/tag");
+const Gitlab = require("../../../app/adapters/gitlab");
+
+const TagsFixture = [{
+    "commit": {
+        "id": "2695effb5807a22ff3d138d593fd856244e155e7",
+        "short_id": "2695effb",
+        "title": "Initial commit",
+        "created_at": "2012-05-27T04:42:42Z",
+        "parent_ids": [
+            "2a4b78934375d7f53875269ffd4f45fd83a84ebe"
+        ],
+        "message": "Initial commit",
+        "author_name": "John Smith",
+        "author_email": "john@example.com",
+        "authored_date": "2012-05-27T04:42:42Z",
+        "committer_name": "Jack Smith",
+        "committer_email": "jack@example.com",
+        "committed_date": "2012-05-27T04:42:42Z"
+    },
+    "release": {
+        "tag_name": "1.0.0",
+        "description": "Amazing release. Wow"
+    },
+    "name": "v1.0.0",
+    "target": "2695effb5807a22ff3d138d593fd856244e155e7",
+    "message": null
+    },
+    {
+        "commit": {
+            "id": "2695effb5807a22ff3d138d593fd856244e155e7",
+            "short_id": "2695effb",
+            "title": "Initial commit",
+            "created_at": "2012-05-27T04:42:42Z",
+            "parent_ids": [
+                "2a4b78934375d7f53875269ffd4f45fd83a84ebe"
+            ],
+            "message": "Initial commit",
+            "author_name": "John Smith",
+            "author_email": "john@example.com",
+            "authored_date": "2012-05-27T04:42:42Z",
+            "committer_name": "Jack Smith",
+            "committer_email": "jack@example.com",
+            "committed_date": "2012-05-27T04:42:42Z"
+        },
+        "release": {
+            "tag_name": "1.0.0",
+            "description": "Amazing release. Wow"
+        },
+        "name": "v1.0.0",
+        "target": "2695effb5807a22ff3d138d593fd856244e155e7",
+        "message": null
+    },
+    {
+        "commit": {
+            "id": "2695effb5807a22ff3d138d593fd856244e155e7",
+            "short_id": "2695effb",
+            "title": "Initial commit",
+            "created_at": "2012-05-27T04:42:42Z",
+            "parent_ids": [
+                "2a4b78934375d7f53875269ffd4f45fd83a84ebe"
+            ],
+            "message": "Initial commit",
+            "author_name": "John Smith",
+            "author_email": "john@example.com",
+            "authored_date": "2012-05-27T04:42:42Z",
+            "committer_name": "Jack Smith",
+            "committer_email": "jack@example.com",
+            "committed_date": "2012-05-27T04:42:42Z"
+        },
+        "release": {
+            "tag_name": "1.0.0",
+            "description": "Amazing release. Wow"
+        },
+        "name": "v1.0.0",
+        "target": "2695effb5807a22ff3d138d593fd856244e155e7",
+        "message": null
+    }
+];
+
 
 describe("Tag library", () => {
     describe("#getLatestAndSecondLatestTagByProjectId", () => {
         const setupCommon = async () => {
-            // Tags endpoint with next page
-            // Mock commit endpoint with branch refs
-
+            jest.mock("../../../app/adapters/gitlab");
+            Gitlab.searchTagsByProjectId.mockResolvedValue();
         }
     });
 });
